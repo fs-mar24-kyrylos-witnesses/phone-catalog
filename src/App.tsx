3fc26637e75a9a26da606689fb4f91c940d3bf97
Yana Momot <yanamomot2004@gmail.com>
2024-06-04 16:54:56 +0300

    feature/adding card on process

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useEffect } from 'react';
 import './App.scss';
 
 import { useProductStore } from './store/productStore';
+// import { Card } from './components/Card/Card';
+// import { Product } from './types/Product';
 
 export const App = () => {
-  const { fetchAllProducts } = useProductStore();
+  const fetchAllProducts = useProductStore(state => state.fetchAllProducts);
 
   useEffect(() => {
     fetchAllProducts();
@@ -18,3 +20,24 @@ export const App = () => {
 };
 
 export default App;
+
+// export const App = () => {
+//   const { fetchAllProducts, catalogProducts } = useProductStore();
+//   const [products, setProducts] = useState<Product[]>([]);
+
+//   useEffect(() => {
+//     setProducts(catalogProducts);
+//     fetchAllProducts();
+//   }, [fetchAllProducts, catalogProducts]);
+
+//   return (
+//     <div className="App">
+//       <h1>Product Catalog</h1>
+//       {products.slice(0, 2).map(product => (
+//         <Card product={product} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default App;
