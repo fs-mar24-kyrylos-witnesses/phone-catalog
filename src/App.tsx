d1c2961e92507cb1681970310b9ea6a2b236ef2e
Dmitriy_Kondratiev <d.kondratiev1@gmail.com>
2024-06-05 14:03:41 +0300

    fixed footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import './App.scss';
 import { useProductStore } from './store/productStore';
-import { Header } from './comonents/Header/Header';
+import { Footer } from './components/Footer';
 
 export const App = () => {
   const fetchAllProducts = useProductStore(state => state.fetchAllProducts);
@@ -13,10 +13,9 @@ export const App = () => {
   return (
     <>
       <div className="App">
-        <Header />
         <h1>Product Catalog</h1>
       </div>
-      <div></div>
+      <Footer />
     </>
   );
 };
