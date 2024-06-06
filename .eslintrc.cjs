d4700f95a8f315b6436a0572fddeceb186e83f45
kinqbert <perehodko1010@gmail.com>
2024-06-06 13:19:32 +0300

    eslint fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,37 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2020: true,
-  },
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended', // Integrates Prettier with ESLint
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prettier'],
+  plugins: ['react-refresh', '@typescript-eslint', 'react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    'prettier/prettier': 'error',
+    'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
+    'react/jsx-uses-react': 'off', // No need to use React in scope with React 17+
+    'react/react-in-jsx-scope': 'off', // No need to use React in scope with React 17+
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        tabWidth: 2,
+        useTabs: false, // Use spaces instead of tabs
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
+    },
   },
 };
