5f33ff013ce6e391cb1c0fe09fc316cf5ebf209b
Yana Momot <yanamomot2004@gmail.com>
2024-06-07 13:41:54 +0300

    add base page product

---
+++ src/types/PerPage.ts
@@ -0,0 +1,8 @@
+// export enum PerPage {
+//   All = 'All',
+//   four = '4',
+//   eight = '8',
+//   sixteen = '16',
+// }
+
+export const PerPage = ['All', '4', '8', '16'];
---
+++ src/types/SortBy.ts
@@ -0,0 +1,8 @@
+// export enum SortBy {
+//   All = 'All',
+//   Alphabet = 'Alphabet',
+//   Newest = 'Newest',
+//   Chepest = 'Chepest',
+// }
+
+export const SortBy = ['All', 'Alphabet', 'Newest', 'Cheapest'];
