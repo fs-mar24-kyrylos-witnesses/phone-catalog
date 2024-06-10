d7ce8e8b2cb6d686ce29ffee5b4a4e7bbca7e988
Ilya Solonetskiy <mr.king101@ukr.net>
2024-06-10 04:45:22 +0300

    add-burger-menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.scss';
+import cn from 'classnames';
 import { useProductStore } from '../../store/productStore';
 import { Link } from 'react-router-dom';
 
@@ -7,6 +8,7 @@ import logo from '../../assets/icons/logo.svg';
 import like from '../../assets/icons/heart.svg';
 import cart from '../../assets/icons/cart-icon.svg';
 import menu from '../../assets/icons/menu.svg';
+import close from '../../assets/icons/close.svg';
 
 export const Header: React.FC = () => {
   const isMenuOpen = useProductStore(state => state.isMenuOpen);
@@ -15,48 +17,62 @@ export const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header__container">
-        <a href="#home">
+        <Link to="">
           <img src={logo} alt="NiceGadgetsLogo" className="header__logo" />
-        </a>
+        </Link>
 
-        <nav className={`header__nav ${isMenuOpen ? 'header__nav--open' : ''}`}>
-          <Link className="header__nav-link" to="#home">
-            HOME
-          </Link>
-          <Link className="header__nav-link" to="#phones">
-            PHONES
-          </Link>
-          <Link className="header__nav-link" to="tablets">
-            TABLETS
-          </Link>
-          <Link className="header__nav-link" to="accessories">
-            ACCESSORIES
-          </Link>
-        </nav>
-      </div>
-
-      <div className="header__icons">
-        <div className="header__icons__container header__icons__container-like">
-          <Link to="#favorites">
-            <img className="header__icons__icon-like" src={like} alt="Like" />
-          </Link>
-        </div>
-        <div className="header__icons__container header__icons__container-cart">
-          <Link to="#cart">
-            <img
-              className="header__icons__icon-cart"
-              src={cart}
-              alt="Shopping Cart"
-            />
-          </Link>
-        </div>
-        <div
-          className="header__icons__container header__icons__container-menu"
-          onClick={toggleMenu}
+        <nav
+          className={cn('nav', {
+            'nav--open': isMenuOpen,
+          })}
         >
-          <Link to="#menu">
-            <img className="header__icons__icon-menu" src={menu} alt="Menu" />
-          </Link>
+          <div className="nav__container">
+            <Link className="nav__container-link" to="#home">
+              HOME
+            </Link>
+
+            <Link className="nav__container-link" to="#phones">
+              PHONES
+            </Link>
+
+            <Link className="nav__container-link" to="tablets">
+              TABLETS
+            </Link>
+
+            <Link className="nav__container-link" to="accessories">
+              ACCESSORIES
+            </Link>
+          </div>
+
+          <div className="nav__icons">
+            <Link
+              className="nav__icons__container nav__icons__container-like"
+              to="#favorites"
+            >
+              <img className="nav__icons__icon-like" src={like} alt="Like" />
+            </Link>
+
+            <Link
+              className="nav__icons__container nav__icons__container-cart"
+              to="#cart"
+            >
+              <img
+                className="nav__icons__icon-cart"
+                src={cart}
+                alt="Shopping Cart"
+              />
+            </Link>
+          </div>
+        </nav>
+
+        <div className="header__menu" onClick={toggleMenu}>
+          <a className="header__menu-link" href="#menu">
+            {isMenuOpen ? (
+              <img className="header__menu-icon" src={close} alt="Close" />
+            ) : (
+              <img src={menu} alt="Menu" />
+            )}
+          </a>
         </div>
       </div>
     </header>
