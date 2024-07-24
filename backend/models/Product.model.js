b041d7016eda58657bc8f569d09b94d306b1b0cf
Yana Momot <yanamomot2004@gmail.com>
2024-07-25 00:12:25 +0300

    unworking version:(

--- backend/models/Product.model.js
+++ backend/models/Product.model.js
@@ -1,50 +1,68 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(require('../config/config.json').development);
 
-const Product = sequelize.define('Product', {
-  id: {
-    type: DataTypes.STRING,
-    primaryKey: true,
+const Product = sequelize.define(
+  'Product',
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    category: DataTypes.STRING,
+    namespaceId: DataTypes.STRING,
+    name: DataTypes.STRING,
+    capacityAvailable: DataTypes.ARRAY(DataTypes.STRING),
+    capacity: DataTypes.STRING,
+    priceRegular: DataTypes.INTEGER,
+    priceDiscount: DataTypes.INTEGER,
+    colorsAvailable: DataTypes.ARRAY(DataTypes.STRING),
+    color: DataTypes.STRING,
+    images: DataTypes.ARRAY(DataTypes.STRING),
+    description: DataTypes.JSONB,
+    screen: DataTypes.STRING,
+    resolution: DataTypes.STRING,
+    processor: DataTypes.STRING,
+    ram: DataTypes.STRING,
+    cell: DataTypes.ARRAY(DataTypes.STRING),
   },
-  category: DataTypes.STRING,
-  namespaceId: DataTypes.STRING,
-  name: DataTypes.STRING,
-  capacityAvailable: DataTypes.ARRAY(DataTypes.STRING),
-  capacity: DataTypes.STRING,
-  priceRegular: DataTypes.INTEGER,
-  priceDiscount: DataTypes.INTEGER,
-  colorsAvailable: DataTypes.ARRAY(DataTypes.STRING),
-  color: DataTypes.STRING,
-  images: DataTypes.ARRAY(DataTypes.STRING),
-  description: DataTypes.JSONB,
-  screen: DataTypes.STRING,
-  resolution: DataTypes.STRING,
-  processor: DataTypes.STRING,
-  ram: DataTypes.STRING,
-  cell: DataTypes.ARRAY(DataTypes.STRING),
-}, {
+  {
     tablename: 'products',
-  }
+  },
 );
 
 async function getProductCards() {
-  return Product.findAll();
-}
-
-// TODO -- add attributes
-async function getProductCardsByCategory(category) {
   return Product.findAll({
-    where: { category },
+    attributes: [
+      'id',
+      'category',
+      'itemId',
+      'name',
+      'fullPrice',
+      'price',
+      'screen',
+      'capacity',
+      'color',
+      'ram',
+      'year',
+      'image',
+    ],
   });
 }
 
-async function getProduct(id) {
-  return Product.findByPk(id)
+// TODO -- add attributes
+// async function getProductCardsByCategory(category) {
+//   return Product.findAll({
+//     where: { category },
+//   });
+// }
+
+async function getProduct(category, id) {
+  return Product.findOne({ where: { category, id } });
 }
 
 module.exports = {
   Product,
   getProductCards,
-  getProductCardsByCategory,
+  // getProductCardsByCategory,
   getProduct,
 };
--- backend/routes/product.route.cjs
+++ backend/routes/product.route.cjs
@@ -4,9 +4,10 @@ const router = express.Router();
 const ProductsController = require('../controllers/product.controller.js');
 const ROUTES = require('../constants/routes.js');
 
-router.get(ROUTES.PRODUCTS_PHONES, ProductsController.getProductCardsByCategory);
-router.get(ROUTES.PRODUCTS_TABLETS, ProductsController.getProductCardsByCategory);
-router.get(ROUTES.PRODUCTS_ACCESSORIES, ProductsController.getProductCardsByCategory);
+router.get(ROUTES.PRODUCTS_ALL, ProductsController.getProductCards);
+// router.get(ROUTES.PRODUCTS_PHONES, ProductsController.getProductCardsByCategory);
+// router.get(ROUTES.PRODUCTS_TABLETS, ProductsController.getProductCardsByCategory);
+// router.get(ROUTES.PRODUCTS_ACCESSORIES, ProductsController.getProductCardsByCategory);
 router.get(ROUTES.PRODUCTS_ID, ProductsController.getProduct);
 
 module.exports = router;
